64588b4be57878d142173d43be37f04242ba67cf
Gleb,Mozhaiskii <gleb.mozhaiskii@kronos.com>
2021-02-04 23:15:50 +0300

    update sql migration

--- src/main/resources/db/migration/V1_1_1__added_roles.sql
+++ src/main/resources/db/migration/V1_1_1__added_roles.sql
@@ -14,7 +14,7 @@ CREATE TABLE user_to_role
     user_id varchar(100),
     role_id varchar(100),
     CONSTRAINT "fk_user_to_role.user" FOREIGN KEY (user_id)
-        REFERENCES client_data (id)
+        REFERENCES client (id)
         ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT "fk_user_to_role.role" FOREIGN KEY (role_id)
         REFERENCES role (id)
